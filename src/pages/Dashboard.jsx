9d99f309dbfb2422f677bd54ee57e3953958bb21
AndrwGmez <andrwgmez68@gmail.com>
2025-03-17 15:37:32 -0500

    Refactor TopBanner and Dashboard components for improved layout and responsiveness

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -34,20 +34,26 @@ const Dashboard = () => {
   const hasNoRoles = !userPermissions.roles || userPermissions.roles.length === 0;
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-1 flex flex-col items-center mt-0">
-        <div className="w-full h-48"> {/* Asegura que el contenedor ocupe todo el ancho y tenga altura fija */}
+        {/* Contenedor del banner */}
+        <div className="w-full h-56 md:h-64 lg:h-72 xl:h-80 relative">
           <HeaderBanner showDashboardBanner={showDashboardBanner} username={username} />
         </div>
-        {hasNoRoles ? (
-          <div className="mt-6 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 2xl:w-10/12 mx-auto bg-red-500 text-white rounded-lg text-center p-4">
+
+        {/* Mensaje de advertencia si no tiene roles */}
+        {hasNoRoles && (
+          <div className="py-10 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 2xl:w-10/12 mx-auto bg-red-500 text-white rounded-lg text-center p-4 z-50">
             <h2 className="text-lg font-bold">⚠️ No tienes roles asignados</h2>
             <p className="text-sm">Por favor, solicita a un administrador que te asigne un rol para continuar.</p>
           </div>
-        ) : (
+        )}
+
+        {/* Contenido principal del dashboard si tiene roles */}
+        {!hasNoRoles && (
           <>
-            <div className="mt-6 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 2xl:w-10/12 mx-auto flex justify-between items-center">
+            <div className="mt-6 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 2xl:w-10/12 mx-auto flex flex-col gap-6 items-center">
               <button
                 className="bg-[#00324D] px-4 py-2 rounded-lg"
                 onClick={() => navigate('/Categorylist')}
