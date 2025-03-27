32f0ed011da92b2d0e8bb9f7bcaf03195f65a214
crisCalambas <cristiansanta5@gmail.com>
2025-03-27 16:56:45 -0500

    logica mejorada seleccion del paso(progreso)

--- src/components/ProgresBar.jsx
+++ src/components/ProgresBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
 // Importaciones de imágenes
 import datosgenerales from '../assets/img/datosgenerales.svg';
 import preguntas from '../assets/img/preguntas.svg';
@@ -8,18 +7,22 @@ import previsualizar from '../assets/img/previsualizar.svg';
 
 const ProgressBar = ({ currentView }) => {
   const navigate = useNavigate();
-
+  
   const viewSteps = {
     SurveyCreate: 1,
     QuestionsCreate: 2,
     PreviewSurvey: 3,
-    // Puedes agregar más vistas según sea necesario
   };
-
+  
   const activeStep = viewSteps[currentView] || 1;
-
+  
   const steps = [
-    { id: 1, label: 'Datos Generales', completed: activeStep >= 1, route: '/SurveyCreate', icon: datosgenerales },
+    { id: 1,
+      label: 'Datos Generales', 
+      completed: activeStep >= 1, 
+      route: '/SurveyCreate', 
+      icon: datosgenerales
+    },
     { id: 2, label: 'Preguntas', completed: activeStep >= 2, route: '/QuestionsCreate', icon: preguntas },
     { id: 3, label: 'Previsualizar', completed: activeStep >= 3, route: '/PreviewSurvey', icon: previsualizar },
   ];
@@ -28,13 +31,6 @@ const ProgressBar = ({ currentView }) => {
     navigate(route);
   };
 
-  const validateActiveStep = () => {
-    if (activeStep === step.id) {
-      return True;
-    }
-    return False;
-  }
-
   return (
     <div className="flex items-center justify-center w-full">
       <div className="flex">
@@ -42,30 +38,28 @@ const ProgressBar = ({ currentView }) => {
           <React.Fragment key={step.id}>
             <div 
               className={`flex items-center relative cursor-pointer ${step.completed ? '' : 'opacity-70'}`}
-              onClick={() => handleStepClick(step.route)}
-            >
-              {/* Contenedor con el fondo rectangular que se extiende detrás del círculo */}
-              <div className={`absolute z-50 -left-12 size-14 shadow-md p-3 border border-4 ${validateActiveStep ? 'bg-yellow-400':'bg-gray-300'} flex items-center justify-center rounded-full w-14 h-14`}> 
+              onClick={() => handleStepClick(step.route)}>
+                
+                {/*círculo con el ícono*/}
+              <div className={`absolute z-50 -left-12 size-14 shadow-md p-3 border border-4 ${activeStep === step.id ? 'bg-yellow-400':'bg-gray-300'} flex items-center justify-center rounded-full w-14 h-14`}> 
                 <img src={step.icon} alt={step.label} className="size-14"/>
               </div>
-              <div 
+
+              {/*rectángulo con el texto del paso*/}
+              <div
                 className={`flex items-center relative ${
-                  activeStep === step.id 
+                  activeStep === step.id
                     ? 'bg-yellow-400' // Fondo amarillo para paso activo
                     : 'bg-gray-300'   // Fondo gris para pasos inactivos
                 }`}
                 style={{ width: '200px' }} // Ancho fijo para todos los rectángulos
-              >
-                {/* Círculo exterior (naranja) con position absolute para que quede por encima del fondo */}
-          
-                  {/* Círculo interior (amarillo o gris) */}
-                  
-            
-                
+                >
+
                 {/* Texto dentro del fondo rectangular, con padding para dejar espacio al círculo */}
                 <div className="py-1 font-bold pl-2 w-full border border-4">
                   {step.label}
                 </div>
+                
               </div>
             </div>
           </React.Fragment>
