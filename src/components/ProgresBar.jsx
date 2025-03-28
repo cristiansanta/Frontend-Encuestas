2280c359a1497c6955e1d9fd634fdeaba1c41205
cristiansanta <cristiansanta5@gmail.com>
2025-03-27 22:43:38 -0500

    logica seleccionar seccion miga pan, cambia color imagen, texto y background

--- src/components/ProgresBar.jsx
+++ src/components/ProgresBar.jsx
@@ -37,20 +37,35 @@ const ProgressBar = ({ currentView }) => {
         {steps.map((step, index) => (
           <React.Fragment key={step.id}>
             <div 
-              className={`flex items-center relative cursor-pointer ${step.completed ? '' : 'opacity-70'}`}
+              className={`flex items-center relative cursor-pointer ${step.completed ? '' : 'opacity-150'}`}
               onClick={() => handleStepClick(step.route)}>
                 
                 {/*círculo con el ícono*/}
-              <div className={`absolute z-50 -left-12 size-14 shadow-md p-3 border border-4 ${activeStep === step.id ? 'bg-yellow-400':'bg-gray-300'} flex items-center justify-center rounded-full w-14 h-14`}> 
-                <img src={step.icon} alt={step.label} className="size-14"/>
+              <div className={`absolute z-50 -left-12 size-14 shadow-md p-3 border border-4 ${
+                (step.completed === true && activeStep > step.id) 
+                ? 'bg-blue-800 '
+                :activeStep === step.id
+                ? 'bg-yellow-400'
+                : 'bg-gray-300'} flex items-center justify-center rounded-full w-14 h-14`}> 
+                <img 
+                src={step.icon} 
+                alt={step.label} 
+                className={`size-14"${
+                  (step.completed === true && activeStep > step.id)
+                  ? 'filter brightness-0 invert' // Hace el ícono blanco cuando el fondo es azul
+                  : ''
+                }`}
+                />
               </div>
 
               {/*rectángulo con el texto del paso*/}
               <div
                 className={`flex items-center relative ${
-                  activeStep === step.id
-                    ? 'bg-yellow-400' // Fondo amarillo para paso activo
-                    : 'bg-gray-300'   // Fondo gris para pasos inactivos
+                  (step.completed === true && activeStep > step.id)
+                    ? 'bg-blue-800 text-white' // Fondo amarillo para paso activo
+                    : activeStep==step.id   // Fondo gris para pasos inactivos
+                    ? 'bg-yellow-400'
+                    : 'bg-gray-300'
                 }`}
                 style={{ width: '200px' }} // Ancho fijo para todos los rectángulos
                 >
