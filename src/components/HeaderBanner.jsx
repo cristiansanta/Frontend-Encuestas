3ad986ac6dd7e646646995717f65bfd02e733bdd
cristiansanta <cristiansanta5@gmail.com>
2025-03-29 09:32:54 -0500

    Mejora de formato y limpieza de código en HeaderBanner y SurveyCreate

--- src/components/HeaderBanner.jsx
+++ src/components/HeaderBanner.jsx
@@ -16,7 +16,7 @@ const HeaderBanner = ({ showDashboardBanner, username }) => {
       setIsMobile(window.innerWidth < 768); // Considerar móvil si el ancho es menor a 768px
     };
 
-    handleResize  (); // Ejecutar al cargar el componente
+    handleResize(); // Ejecutar al cargar el componente
     window.addEventListener('resize', handleResize); // Escuchar cambios en el tamaño de la ventana
 
     return () => {
@@ -29,7 +29,7 @@ const HeaderBanner = ({ showDashboardBanner, username }) => {
     ? isMobile
       ? dashboardresponsive
       : dashboardImage
-    : bannerImage;
+    : dashboardImage;
 
   return (
     <div className={`banner-container ${showDashboardBanner ? 'svg-container' : ''} relative`}>
